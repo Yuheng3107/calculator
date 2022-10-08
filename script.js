fac95b594bb5e5dc466f7a644e01ff2df815fa00
Kuang Yu Heng <yuheng3107@gmail.com>
2022-10-08 23:18:58 +0800

    Made Prototype for Calculator

---
+++ script.js
@@ -0,0 +1,67 @@
+ 
+ const add = (a,b) => +a + +b;
+ const subtract = (a,b) => +a - +b;
+ const multiply = (a,b) => +a*+b;
+ const divide = (a,b) => parseFloat(+a/+b).toFixed(2);
+
+ function operate(operator, a, b) {
+     if (operator === '+') return add(a,b);
+     else if (operator === '-') return subtract(a,b);
+     else if (operator === 'x') return multiply(a,b);
+     else if (operator === '÷') return divide(a,b);
+ }
+
+ function operatorCheck(text) {
+    if (operator !== "") {
+        bottomNum = operate(operator, topNum, bottomNum); 
+    }
+    operator = text;
+    topNum = bottomNum;
+    bottomNum = 0;
+ }
+ const buttons = document.querySelectorAll('button');
+ const bottom = document.querySelector('#bottom');
+ const above = document.querySelector('#top');
+ let operator = "";
+ let bottomNum = "";
+ let topNum = "";
+ buttons.forEach(button => button.addEventListener('click', (e) => {
+     const text = e.target.textContent;
+     num = Number(text);
+    
+     if (!isNaN(num)) {
+            if (bottomNum == 0) bottomNum = "";
+            bottomNum += num;
+         
+     }
+     else {
+        if (text === 'CLEAR') {
+            bottom.textContent = "";
+            above.textContent = "";
+            bottomNum = "";
+            operator = "";
+        }
+        else if (text === 'DELETE') bottomNum = bottomNum.slice(0,bottomNum.length-1);
+
+        else if (text === '=') {
+            if (operator !== "") {
+                bottomNum = operate(operator, topNum, bottomNum);
+                operator = ""; 
+                topNum = "";
+            }
+            
+        }
+        else if (text === '.') {
+
+        }
+        else {
+            
+            operatorCheck(text);
+
+        };
+     }
+
+     bottom.textContent = bottomNum;
+     above.textContent = topNum + ` ${operator}`;
+ }))
+
